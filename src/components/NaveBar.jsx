1d87174f8c60a34dc41e733c32f586ae30dab856
Adarsh nm <adarshnm02@gmail.com>
2024-05-02 21:19:34 +0530

    Initial Commit

---
+++ src/components/NaveBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NaveBar = () => {
+  return (
+    <div>
+      <h1 className='text-white'>NETFLIX</h1>
+      <div>
+        <button className='text-white'>Sign In</button>
+        <button className='text-white'>Sign Up</button>
+      </div>
+    </div>
+  )
+}
+
+export default NaveBar
