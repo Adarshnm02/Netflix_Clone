ed88d73e4a257a24056f0940e23738b7245c6cb8
Adarsh nm <adarshnm02@gmail.com>
2024-05-04 00:14:41 +0530

    Row modified in Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,8 +10,8 @@ function Home() {
       <Row title='Up Coming' fetchURL={requests.requestUpcoming}/>
       <Row title='Popular' fetchURL={requests.requestPopular}/>
       <Row title='Top Rated' fetchURL={requests.requestTopRated}/>
-      <Row title='Horror' fetchURL={requests.requestHorror}/>
-      <Row title='Comedy' fetchURL={requests.requestComedy}/>
+      {/* <Row title='Horror' fetchURL={requests.requestHorror}/>
+      <Row title='Comedy' fetchURL={requests.requestComedy}/> */}
 
     
     </div>
