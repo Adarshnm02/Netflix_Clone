9c4c505a249a3dc0baeca9216182c79d9e664eae
Adarsh nm <adarshnm02@gmail.com>
2024-05-04 14:27:49 +0530

    SignUp page Design complited

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
-import NaveBar from "./components/NaveBar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import { AuthContextProvider } from "./context/AuthContext";
+import NaveBar from "./components/NaveBar";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUp";
+import Account from "./Pages/Account";
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
         <NaveBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />}/>
+          <Route path="/signup" element={<SignUp />}/>
+          <Route path="/account" element={<Account />}/>
+
         </Routes>
       </AuthContextProvider>
     </>
