9550ff2ea3d1d1938ad3b3fb1c0954765c43d0c5
Adarsh nm <adarshnm02@gmail.com>
2024-05-04 18:09:39 +0530

    ch

--- src/Requests.js
+++ src/Requests.js
@@ -5,9 +5,6 @@ const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`,
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}&language=en-US&page=1`,
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&language=en-US&page=1`,
-    requestHorror: `https://api.themoviedb.org/3/discover/movie?api_key=YOUR_API_KEY_HERE&include_adult=true&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=Horror`,
-    requestComedy: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&include_adult=true&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=Comedy`
-
 };
 
 
